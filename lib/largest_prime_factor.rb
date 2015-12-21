cd4a7648280f5526def2ff5e3f1df0748e599194
retraido <enzosarin@gmail.com>
2015-12-21 13:44:34 +0000

    nitrous-backup at Mon Dec 21 13:44:34 UTC 2015

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,17 @@
-# Enter your procedural solution here!
+def largest_prime_factor(limit)
+primez = []
+mo_primez = []
+(1..limit).each do |check|
+    if limit % check == 0
+        then primez << check
+    end
+    end
+    primez.each do |crunch|
+        if crunch == 2 || crunch == 3 || crunch == 5 || crunch == 7
+            then mo_primez << crunch
+        elsif crunch % 13 != 0 && crunch % 2 != 0 && crunch % 3 != 0 && crunch % 5 != 0 && crunch % 7 != 0
+    then mo_primez << crunch
+end
+end
+    return mo_primez.max
+end
